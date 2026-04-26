c5fcfb3af0648cc1ee0de842c6b65b09c840c1c6
MaxRoenco <maxroenco2004@gmail.com>
2026-04-26 21:26:14 +0300

    chore(lab4): scaffold eleventy project

--- lab4/.eleventy.js
+++ lab4/.eleventy.js
@@ -1,15 +1,37 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/css");
-  eleventyConfig.addPassthroughCopy("src/admin");
-  eleventyConfig.addPassthroughCopy("src/images");
-  // Pass through the src directory but we will build from src and output to _site
+  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+  eleventyConfig.addPassthroughCopy({ "src/admin": "admin" });
+
+  eleventyConfig.addCollection("home", function (collectionApi) {
+    return collectionApi.getFilteredByTag("home");
+  });
+
+  eleventyConfig.addCollection("courses", function (collectionApi) {
+    return collectionApi
+      .getFilteredByTag("course")
+      .sort((a, b) => (a.data.order || 0) - (b.data.order || 0));
+  });
+
+  eleventyConfig.addCollection("testimonials", function (collectionApi) {
+    return collectionApi
+      .getFilteredByTag("testimonial")
+      .sort((a, b) => (a.data.order || 0) - (b.data.order || 0));
+  });
+
+  eleventyConfig.addCollection("galleryItems", function (collectionApi) {
+    return collectionApi
+      .getFilteredByTag("galleryItem")
+      .sort((a, b) => (a.data.order || 0) - (b.data.order || 0));
+  });
 
   return {
     dir: {
       input: "src",
-      output: "_site",
       includes: "_includes",
-      data: "_data"
+      data: "_data",
+      output: "_site",
     },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
   };
 };
